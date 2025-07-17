502d09a7271fa24fcaf31ecca4b30db6c657a382
Zozo <gulabsinghrajput008@gmail.com>
2025-07-17 21:16:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,10 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN', '7758524025:AAEVf_OePVQ-6hhM1GfvRlqX3QZI
 DUMP_CHAT_ID = int(os.environ.get('DUMP_CHAT_ID', '-1002030723564'))
 FSUB_ID = int(os.environ.get('FSUB_ID', '-1002237630219'))
 
+#mongodb 
+
+#MONGO_URL = os.getenv("MONGO_URL", "")
+
 # User Session
 USER_SESSION_STRING = os.environ.get('USER_SESSION_STRING', '')
 if not USER_SESSION_STRING:
