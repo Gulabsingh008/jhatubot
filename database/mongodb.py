25a583018d8af556c7e977da35fcf285c3ff7414
Zozo <gulabsinghrajput008@gmail.com>
2025-07-18 14:05:59 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,35 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from config import MONGO_URL
 
-client = AsyncIOMotorClient(MONGO_URL)
-db = client['terabox_bot']
-users_collection = db['users']
+MONGO_URI = ""
+mongo_client = AsyncIOMotorClient(MONGO_URI)
+db = mongo_client["terabox_db"]
+users_collection = db["users"]
 
-async def add_user(user_id, name):
-    user = await users_collection.find_one({'user_id': user_id})
-    if not user:
+async def save_user(user_id, username):
+    existing = await users_collection.find_one({"user_id": user_id})
+    if not existing:
         await users_collection.insert_one({
-            'user_id': user_id,
-            'name': name,
-            'is_banned': False
+            "user_id": user_id,
+            "username": username
         })
-
-async def is_banned(user_id):
-    user = await users_collection.find_one({'user_id': user_id})
-    return user and user.get('is_banned', False)
-
-async def ban_user(user_id):
-    await users_collection.update_one({'user_id': user_id}, {'$set': {'is_banned': True}})
-
-async def unban_user(user_id):
-    await users_collection.update_one({'user_id': user_id}, {'$set': {'is_banned': False}})
-
-async def get_all_users():
-    users = users_collection.find()
-    return await users.to_list(length=10000)
-
-async def get_total_users():
-    return await users_collection.count_documents({})
-
-async def get_banned_users():
-    return await users_collection.count_documents({'is_banned': True})
